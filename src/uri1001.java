4979ad052d3f8961bf0f17c047703a3f85e1755f
Tiago aparecido pereira <59799134+thyagoman@users.noreply.github.com>
2023-04-18 18:57:52 -0300

    Soma de 2 input com 1 output
    Soma de 2 input com 1 output

--- src/uri1001.java
+++ src/uri1001.java
@@ -5,11 +5,11 @@ public class uri1001 {
 
 	public static void main(String[] args) {		
 
-		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
+		Scanner input = new Scanner(System.in);
+		int A = input.nextInt();
+		int B = input.nextInt();
 		int X = A + B;
 		System.out.println("X = " + X);
-		sc.close();
+		input.close();
 	}
 }
