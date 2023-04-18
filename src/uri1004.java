d350cf027a6968f8d48cadae79f5aeb7a97250bd
Tiago aparecido pereira <59799134+thyagoman@users.noreply.github.com>
2023-04-18 19:03:10 -0300

    Produto Simples
    Produto Simples

--- src/uri1004.java
+++ src/uri1004.java
@@ -7,17 +7,17 @@ public class uri1004 {
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		
 		int x, y, prod;
 		
-		x = sc.nextInt();
-		y = sc.nextInt();
+		x = in.nextInt();
+		y = in.nextInt();
 		
 		prod = x*y;
 		
 		System.out.println("PROD = " + prod);
 		
-		sc.close();
+		in.close();
 	}
 }
