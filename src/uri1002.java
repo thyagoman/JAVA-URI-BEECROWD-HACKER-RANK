f21cb47da08d474a933ef40b2a0a79f08c1eb462
Tiago aparecido pereira <59799134+thyagoman@users.noreply.github.com>
2023-04-18 18:59:35 -0300

    Area de um circulo
    Area de um circulo

--- src/uri1002.java
+++ src/uri1002.java
@@ -7,16 +7,16 @@ public class uri1002 {
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
+		Scanner in= new Scanner(System.in);
 		
 		double R, A, pi = 3.14159;
 	    
-	    R = sc.nextDouble();
+	    R = in.nextDouble();
 
 	    A = pi * R * R;
 
 	    System.out.printf("A=%.4f%n", A);
 
-		sc.close();
+		in.close();
 	}
 }
